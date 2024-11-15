df9b651ffded21cfdead28c3d4fac08534ba45e1
neko-Koi <nekoxiaoxiao@gmail.com>
2024-11-15 23:22:04 +0800

    Site updated: 2024-11-15 23:22:03

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/11/15/hello-world/"];function toRandomPost(){
+var posts=["2024/11/15/一条小鱼写给你的一封信/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
