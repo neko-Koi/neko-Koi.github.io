2bd1dfe29a99d1251a68578f8a034feb99b4f1af
neko-Koi <nekoxiaoxiao@gmail.com>
2024-11-20 17:18:53 +0800

    Site updated: 2024-11-20 17:18:53

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/11/15/一条小鱼写给你的一封信/","2024/11/20/分析「你在学校期间因何事一举成名全校皆知？」/","2024/11/20/对人类笑点的简单观察/"];function toRandomPost(){
+var posts=["2024/11/15/一条小鱼写给你的一封信/","2024/11/20/对人类笑点的简单观察/","2024/11/20/分析「你在学校期间因何事一举成名全校皆知？」/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
